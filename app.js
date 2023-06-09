004d8506ab1681ce1fb1ada7f5811b0b94c0eb83
codewithtuba <101013919+codewithtuba@users.noreply.github.com>
2023-06-09 21:13:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+function bubbles() {
+    let count = 400;
+    let section = document.querySelector('section')
+    let i = 0;
+    while( i <= count) {
+        let bubble = document.createElement('i')
+        let x = Math.floor(Math.random() * window.innerWidth)
+        let y = Math.floor(Math.random() * window.innerHeight)
+
+        let size = Math.random() * 15;
+        bubble.style.left = x + 'px'
+        bubble.style.top = y + 'px'
+        bubble.style.width = 1 + size + 'px';
+        bubble.style.height = 1 + size + 'px';
+
+        bubble.style.animationDuration = 5 + size + 's';
+        bubble.style.animationDelay = -size + 's';
+        section.appendChild(bubble);
+        i++;
+    }
+}
+bubbles();
